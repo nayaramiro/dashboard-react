d6f18294544ee819b258fb45f432d203395f0397
nayaramiro <nayaramiro.be@gmail.com>
2025-02-20 16:12:39 +0100

    create-nav navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useContext } from "react";
 import Header from "./components/Header";
 import { ThemeContext, ThemeProvider } from "./contexts/ThemeContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   const { theme } = useContext(ThemeContext);
 
   return (
     <div
-      className={`h-screen ${theme === 'light' ? 'bg-blue-100' : 'bg-blue-950'}`}
+      className={`h-screen ${
+        theme === "light" ? "bg-blue-100" : "bg-blue-950"
+      } p-4 grid grid-cols-2`}
     >
-      <h1 className="underline">Hello</h1>
-      <Header />
+      <Navbar />
+      <div className="nav">
+        <Header />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div id="navbar" className="w-2xs h-full rounded-xl bg-blue-200"></div>
+  );
+}
